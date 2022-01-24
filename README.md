0563d23b6f8eaec66504c0601f731012f43eb7ce
lwei88 <wongliangwei88@gmail.com>
2022-01-25 04:13:04 +0800

    retry mq connection while waiting the rabbitmq finished startup add docker compose added running guide

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # emailing-service-demo
+
+## Prerequisite Requirements
+
+- Docker
+- docker-compose
+
+## How to run the application
+
+1. Make sure you have the Docker and docker-compose installed, please find the [guide](https://docs.docker.com/get-docker/)
+2. Clone the repo via the command below:
+
+```
+git clone https://github.com/lwei88/emailing-service-demo.git
+```
+
+3. Start the application by running the command below:
+
+```
+docker-compose up -d --build
+```
--- src/utils/messageQueue/worker.ts
+++ src/utils/messageQueue/worker.ts
@@ -7,44 +7,56 @@ class Worker<T> {
     this.startWork = startWork;
   }
 
-  static async onMessage<U>(channel: amqp.Channel, startWork: (args: U) => void, msg: amqp.ConsumeMessage | null) {
-    if (msg)
-      try {
-        const timeStamp = Date.now();
+  static async onMessage<T>(channel: amqp.Channel, startWork: (args: T) => void, msg: amqp.ConsumeMessage | null) {
+    if (msg) {
+      const timeStamp = Date.now();
 
-        console.log(`[*] - ${new Date().toISOString()} - Received queue (${timeStamp})`);
-        await startWork(JSON.parse(msg.content.toString()));
-        channel.ack(msg);
-        console.log(`[*] - ${new Date().toISOString()} - Done (${timeStamp})`);
-      } catch (error: any) {
-        // nack the message, will become dead-letter which will be throw to standby queue
-        channel.nack(msg, false, false);
-        console.log('Error occured: %s', error.message);
-      }
+      console.log(`[*] - ${new Date().toISOString()} - Received queue (${timeStamp})`);
+      await startWork(JSON.parse(msg.content.toString()));
+      channel.ack(msg);
+      console.log(`[*] - ${new Date().toISOString()} - Done (${timeStamp})`);
+    }
+  }
+
+  static async consumeQueue<T>(messageQueue: string, channel: amqp.Channel, startWork: (args: T) => void) {
+    await channel.assertExchange('default', 'direct', { durable: false });
+    await channel.assertQueue(messageQueue, {
+      durable: true,
+    });
+    await channel.bindQueue(messageQueue, 'default', messageQueue);
+    await channel.prefetch(1);
+
+    const onMessageCb = (msg: amqp.ConsumeMessage | null): void => {
+      Worker.onMessage<T>(channel, startWork, msg);
+    };
+
+    console.log('[*] Waiting for messages in %s. To exit press CTRL+C', messageQueue);
+    await channel.consume(messageQueue, onMessageCb, { noAck: false, arguments: { redelivered: false } });
   }
 
   async start(messageQueue: string) {
-    let connection = null;
-    try {
-      connection = await amqp.connect(process.env.MESSAGEQUEUE_CONNECTION as string);
-      const channel = await connection.createChannel();
-      await channel.assertExchange('default', 'direct', { durable: false });
+    let channel: amqp.Channel;
 
-      await channel.assertQueue(messageQueue, {
-        durable: true,
-      });
-      await channel.bindQueue(messageQueue, 'default', messageQueue);
-      await channel.prefetch(1);
-
-      const onMessageCb = (msg: amqp.ConsumeMessage | null): void => {
-        Worker.onMessage<T>(channel, this.startWork, msg);
-      };
-
-      console.log('[*] Waiting for messages in %s. To exit press CTRL+C', messageQueue);
-      await channel.consume(messageQueue, onMessageCb, { noAck: false, arguments: { redelivered: false } });
-    } catch (error) {
-      console.error('Error occured: %s', error);
+    async function init(mqConn: string): Promise<amqp.Channel> {
+      const connection = await amqp.connect(mqConn);
+      return connection.createChannel();
     }
+
+    const retryConnection = setInterval(async () => {
+      console.log('Connecting to MQ....');
+      if (!channel) {
+        try {
+          channel = await init(process.env.MESSAGEQUEUE_CONNECTION as string);
+        } catch (e: any) {
+          console.log(e.message);
+        }
+      } else {
+        clearInterval(retryConnection);
+        console.log('Connected to MQ');
+
+        await Worker.consumeQueue<T>(messageQueue, channel, this.startWork);
+      }
+    }, 5_000);
   }
 }
 
